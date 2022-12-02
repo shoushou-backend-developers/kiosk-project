f621ae9a5c72cce7343a16ff76a94b2a8cd9d94b
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-12-02 19:46:31 +0900

    #1 - 스프링 부트 프로젝트 시작
    키오스크 프로젝트 진행을 위한 스프링 부트 프로젝트 생성

---
+++ src/main/java/com/shoushoubackenddeveloper/kiosk_project/KioskProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shoushoubackenddeveloper.kiosk_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoushoubackenddeveloper/kiosk_project/KioskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoushoubackenddeveloper.kiosk_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
