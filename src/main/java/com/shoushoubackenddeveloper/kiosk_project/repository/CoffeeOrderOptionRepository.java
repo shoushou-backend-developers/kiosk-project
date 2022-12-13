f7d786f57c62da78fbd537b5a3d20a8928b860fa
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-12-13 14:11:56 +0900

    리포지토리 수정

--- src/main/java/com/shoushoubackenddeveloper/kiosk_project/repository/CoffeeOrderOptionRepository.java
+++ src/main/java/com/shoushoubackenddeveloper/kiosk_project/repository/CoffeeOrderOptionRepository.java
@@ -4,7 +4,10 @@ import com.shoushoubackenddeveloper.kiosk_project.domain.CoffeeOrderOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CoffeeOrderOptionRepository extends JpaRepository<CoffeeOrderOption, Long> {
 
+    List<CoffeeOrderOption> findByCoffeeOrderId(Long coffeeOrderId);
 }
--- src/main/java/com/shoushoubackenddeveloper/kiosk_project/repository/OptionRepository.java
+++ src/main/java/com/shoushoubackenddeveloper/kiosk_project/repository/OptionRepository.java
@@ -4,8 +4,10 @@ import com.shoushoubackenddeveloper.kiosk_project.domain.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
-
+    List<Option> findByCoffeeOrderOption_Id(Long coffeeOrderOptionId);
 }
